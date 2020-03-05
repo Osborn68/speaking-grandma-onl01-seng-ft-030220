5359e1175878e491d5a366dd39da0031977fefd3
Christopher Osborn <z68ozz@gmail.com>
2020-03-05 19:14:42 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -3,8 +3,15 @@ def speak_to_grandma(phrase)
     return "I LOVE YOU TOO PUMPKIN!"
   elsif phrase != phrase.upcase()
     return  "HUH?! SPEAK UP, SONNY!"
+<<<<<<< HEAD
  else 
   return "NO, NOT SINCE 1938!"
  end  
 end 
   
+=======
+else 
+  return "NO, NOT SINCE 1938!"
+  
+END 
+>>>>>>> 5b77243f671033c9dc70553f2cf2c4976fbf7014
